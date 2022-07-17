17226a7b22da0a94243b6c1e0d4fe6eec054e8dd
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-07-16 20:46:49 -0700

    trying to get save icon

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function dayplanner() {
         var savebutton = $("<button>").addClass("saveBtn");
         timeblock.append(savebutton);
 
-        var saveBtn = $("<i>").addClass("fa-solid fa-floppy-disk");
+        var saveBtn = $("<i>").addClass("fa fa-save");
         savebutton.append(saveBtn);
 
         // This function will make each hour a certain color to represent if it is in the present, past, or future.
