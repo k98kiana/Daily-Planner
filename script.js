f8e6b863f28ff1df8cae61a1a267846f764cec9d
k98kiana <105411392+k98kiana@users.noreply.github.com>
2022-07-16 18:28:12 -0700

    adding script file

---
+++ script.js
@@ -0,0 +1,25 @@
+// The current day will be the first thing displayed.
+$("#currentDay").text(new Date());
+
+// Creating variables so program can recognize the current time, and have hours to reporesent a standard 9 to 5.
+var currenthour = new Date().getHours();
+var standardhours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+function dayplanner() {
+    for (var i = 0; i < standardhours.length; i++) {
+        createtimeblock();
+    }
+
+    function createtimeblock(){
+
+        //Creating area for the time blocks to be in.
+        var timeblock = $("<div>").addClass("time row");
+        $("main").append(timeblock);
+
+        //Creating the time block and appending it to the section.
+        var hour = $("<div>").addClass("hour");
+        timeblock.append(hour);
+
+        hour.text(standardhours[i] + ":00");
+    }
+}
